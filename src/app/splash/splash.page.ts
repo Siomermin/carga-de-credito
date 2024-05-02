4f21f75a2cc204bbfe83db2e65b02ec21881e084
Siomermin <fermingonzalez011@gmail.com>
2024-05-02 11:51:02 -0300

    Entrega clase 03 - Login funcional + Icon + Splash

---
+++ src/app/splash/splash.page.ts
@@ -0,0 +1,33 @@
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-splash',
+  templateUrl: './splash.page.html',
+  styleUrls: ['./splash.page.scss'],
+})
+export class SplashPage implements OnInit {
+
+  public router = inject(Router);
+  public renderer = inject(Renderer2);
+
+  @ViewChild('h1Element') h1Element!: ElementRef;
+  @ViewChild('h3Element') h3Element!: ElementRef;
+  @ViewChild('imgElement') imgElement!: ElementRef;
+
+  ngOnInit(): void {
+
+    setTimeout(() => {
+      // this.renderer.addClass(this.h1Element.nativeElement, 'tracking-in-expand');
+      // this.renderer.addClass(this.h3Element.nativeElement, 'tracking-in-expand');
+      this.renderer.addClass(this.imgElement.nativeElement, 'rotate-horizontal-center');
+    }, 1000); // Add classes after 1 second (1000 milliseconds)
+
+    setTimeout(() => {
+      this.router.navigateByUrl('auth/login');
+    }, 3000);
+
+
+  }
+
+}
